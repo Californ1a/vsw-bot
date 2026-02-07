0500b52b0fe546a7816ded892b19b96a297461d8
Californ1a <rk_california@yahoo.com>
2026-02-06 23:32:43 -0500

    move chunks to own functions

--- src/createVideos.ts
+++ src/createVideos.ts
@@ -3,6 +3,121 @@ import { bot, log } from './util/bot';
 import mediaTemplate from '../templates/media';
 import { fetchAllVideos } from './util/youtube';
 import { cleanTitle, FALSE_SUBPAGE_REASON } from './util/titleinfo';
+import type { youtube_v3 } from '@googleapis/youtube';
+
+const TEMP_FILES: Bun.BunFile[] = [];
+
+async function saveLastTime(video: youtube_v3.Schema$Video, file: Bun.BunFile) {
+	if (video.snippet?.publishedAt) {
+		const publishedAt = new Date(video.snippet.publishedAt);
+		await file.write(publishedAt.toISOString());
+	}
+}
+
+async function createPage(video: youtube_v3.Schema$Video, titleInfo: VideoTitleInfo, url: string) {
+	log(`[I] Creating page for video: ${titleInfo.title} (${url})`);
+	// Format data
+	let duration = '';
+	if (video.contentDetails?.duration) {
+		const parsed = parse(video.contentDetails.duration);
+		const hrs = (parsed.hours || 0).toString().padStart(2, '0');
+		const mins = (parsed.minutes || 0).toString().padStart(2, '0');
+		const secs = (parsed.seconds || 0).toString().padStart(2, '0');
+		duration = `${hrs}:${mins}:${secs}`;
+	}
+	let date = '';
+	if (video.snippet?.publishedAt) {
+		date = (new bot.Date(video.snippet.publishedAt)).format('D MMMM YYYY', 'utc');
+	}
+	let description = '';
+	if (video.snippet?.description) {
+		description = video.snippet.description
+			.replace(/\u200E/g, '') // Remove any LRM characters
+			.replace(/[“”]/g, '"') // Replace curly quotes with straight quotes
+			.replace(/[‘’]/g, "'") // Replace curly apostrophes with straight apostrophes
+			.replace(/(?<!\n)\n(?!\n)/g, '<br>\n') // Add br tags for single newlines
+			.replace(/\n([#*])/g, '\n<nowiki/>$1') // Prevent lists from being created
+	}
+
+	// Create page content
+	let content = mediaTemplate;
+	content = content.replace(/NAME/g, titleInfo.originalTitle)
+		.replace(/MEDIA/g, titleInfo.mediaTitle)
+		.replace(/DATE/g, date)
+		.replace(/TYPE/g, 'video')
+		.replace(/DURATION/g, duration)
+		.replace(/YOUTUBE_URL/g, url)
+		.replace(/DESCRIPTION/g, description);
+
+	if (titleInfo.restrictedTitleReasons.length > 0) {
+		const restrictionStr = titleInfo.restrictedTitleReasons.join('; ');
+		log(`[W] Title "${titleInfo.originalTitle}" has restrictions: ${restrictionStr}`);
+		content += '\n{{Restricted title'
+					+ '|' + titleInfo.restrictedTitleReasons.join('|')
+					+ (titleInfo.restrictedTitleReasons.includes(FALSE_SUBPAGE_REASON) ? '|subpage=1' : '')
+					+ '}}';
+		if (titleInfo.originalTitle !== titleInfo.title) {
+			content = `{{DISPLAYTITLE:${titleInfo.originalTitle}}}\n` + content;
+		}
+	}
+
+	// Create page
+	try {
+		if (process.env.NODE_ENV === 'production') {
+			await bot.create(titleInfo.title, content, `Automated creation of page for YouTube video ${url}`);
+		} else {
+			log(`[S] (Simulated) Created page: ${titleInfo.title}`);
+		}
+	} catch (error) {
+		log(`[E] Error creating page for video: ${titleInfo.title}`);
+		log(error);
+	}
+}
+
+async function uploadThumbnail(video: youtube_v3.Schema$Video, titleInfo: VideoTitleInfo, url: string) {
+	// Upload thumbnail image
+	log(`[I] Uploading video's thumbnail image: ${titleInfo.mediaTitle}`);
+	const thumbs = video.snippet?.thumbnails || {};
+	let best: youtube_v3.Schema$Thumbnail | undefined;
+	let bestResolution = 0;
+	for (const value of Object.values(thumbs)) {
+		const thumb = value as youtube_v3.Schema$Thumbnail | undefined;
+		if (!thumb?.url) continue;
+
+		const w = thumb.width || 0;
+		const h = thumb.height || 0;
+		const resolution = w * h;
+		if (resolution > bestResolution) {
+			bestResolution = resolution;
+			best = thumb;
+		}
+	}
+	if (!best?.url) return;
+	
+	const imageUrl = best.url;
+	const tempFilename = `thumbnail-${Date.now()}.jpg`;
+	const file = Bun.file(tempFilename);
+	TEMP_FILES.push(file);
+	try {
+		const result = await fetch(imageUrl);
+		await Bun.write(file, result);
+		if (process.env.NODE_ENV === 'production') {
+			await bot.upload(tempFilename, `${titleInfo.mediaTitle}.jpg`, `{{Media thumbnail|link=${imageUrl}}}`, {
+				ignorewarnings: false,
+				comment: `Automated upload of thumbnail image for YouTube video ${url}`,
+			});
+			await Bun.sleep(1000);
+		} else {
+			log(`[S] (Simulated) Uploaded image: ${imageUrl}`);
+		}
+	} catch (error) {
+		if (error instanceof Error && error.message.includes('already exists')) {
+			log(`[I] Image already exists: ${imageUrl}`);
+		}
+		log(`[E] Error uploading image: ${imageUrl}`);
+		log(error);
+	}
+}
 
 let count = 0;
 async function main() {
@@ -12,7 +127,7 @@ async function main() {
 	if (exists) {
 		const content = await file.text();
 		if (content) {
-			lastCreated = new Date(content);
+			lastCreated = new Date(content.trim());
 		}
 	}
 	const utcStr = lastCreated ? lastCreated.toISOString() : 'None';
@@ -76,7 +191,7 @@ async function main() {
 
 	// Create pages for videos
 	for (const video of videos) {
-		if (count >= 5) {
+		if (count >= 3) {
 			break;
 		}
 		count++;
@@ -89,121 +204,44 @@ async function main() {
 		}
 		if (titleInfo.title === '') {
 			log(`[W] Skipping video with empty title: ${url}`);
+			await saveLastTime(video, file);
 			continue;
 		}
 		if (duplicateTitles.get(titleInfo.title)?.length! > 1) {
 			log(`[W] Skipping video with duplicate title: ${titleInfo.title} (${url})`);
+			await saveLastTime(video, file);
 			continue;
 		}
 
-		// Skip existing pages
+		// Check page existance
 		try {
 			const page = new bot.Page(titleInfo.title);
 			const exists = await page.exists();
 			if (exists) {
-				log(`[W] Skipping video with existing page: ${titleInfo.title} (${url})`);
-				continue;
+				log(`[W] Page for video already exists: ${titleInfo.title} (${url})`);
+			} else {
+				await createPage(video, titleInfo, url);
 			}
 		} catch (error) {
 			log(`[E] Error checking existence of page: ${titleInfo.title}`);
 			log(error);
-			continue;
-		}
-		
-		// Format data
-		let duration = '';
-		if (video.contentDetails?.duration) {
-			const parsed = parse(video.contentDetails.duration);
-			const hrs = (parsed.hours || 0).toString().padStart(2, '0');
-			const mins = (parsed.minutes || 0).toString().padStart(2, '0');
-			const secs = (parsed.seconds || 0).toString().padStart(2, '0');
-			duration = `${hrs}:${mins}:${secs}`;
-		}
-		let date = '';
-		if (video.snippet?.publishedAt) {
-			date = (new bot.Date(video.snippet.publishedAt)).format('D MMMM YYYY', 'utc');
-		}
-		let description = '';
-		if (video.snippet?.description) {
-			description = video.snippet.description
-				.replace(/\u200E/g, '') // Remove any LRM characters
-				.replace(/[“”]/g, '"') // Replace curly quotes with straight quotes
-				.replace(/[‘’]/g, "'") // Replace curly apostrophes with straight apostrophes
-    		.replace(/(?<!\n)\n(?!\n)/g, '<br>\n') // Add br tags for single newlines
-    		.replace(/\n([#*])/g, '\n<nowiki/>$1') // Prevent lists from being created
 		}
 
-		// Create page content
-		let content = mediaTemplate;
-		content = content.replace(/NAME/g, titleInfo.originalTitle)
-			.replace(/MEDIA/g, titleInfo.mediaTitle)
-			.replace(/DATE/g, date)
-			.replace(/TYPE/g, 'video')
-			.replace(/DURATION/g, duration)
-			.replace(/YOUTUBE_URL/g, url)
-			.replace(/DESCRIPTION/g, description);
-
-		if (titleInfo.restrictedTitleReasons.length > 0) {
-			const restrictionStr = titleInfo.restrictedTitleReasons.join('; ');
-			log(`[W] Title "${titleInfo.originalTitle}" has restrictions: ${restrictionStr}`);
-			content += '\n{{Restricted title'
-						+ '|' + titleInfo.restrictedTitleReasons.join('|')
-						+ (titleInfo.restrictedTitleReasons.includes(FALSE_SUBPAGE_REASON) ? '|subpage=1' : '')
-						+ '}}';
-			if (titleInfo.originalTitle !== titleInfo.title) {
-				content = `{{DISPLAYTITLE:${titleInfo.originalTitle}}}\n` + content;
-			}
-		}
-
-		// Create page
-		log(`[I] Creating page for video: ${titleInfo.title} (${url})`);
+		// Check file page existance
 		try {
-			if (process.env.NODE_ENV === 'production') {
-				await bot.create(titleInfo.title, content, `Automated creation of page for YouTube video ${url}`);
+			const page = new bot.Page(`File:${titleInfo.mediaTitle}.jpg`);
+			const exists = await page.exists();
+			if (exists) {
+				log(`[W] File page already exists: File:${titleInfo.mediaTitle}.jpg (${url})`);
 			} else {
-				log(`[S] (Simulated) Created page: ${titleInfo.title}`);
+				await uploadThumbnail(video, titleInfo, url);
 			}
 		} catch (error) {
-			log(`[E] Error creating page for video: ${titleInfo.title}`);
+			log(`[E] Error checking existence of file page: File:${titleInfo.mediaTitle}.jpg`);
 			log(error);
-			continue;
 		}
 
-		// Upload thumbnail image
-		log(`[I] Uploading video's thumbnail image: ${titleInfo.mediaTitle}`);
-		let imageUrl = video.snippet?.thumbnails?.maxres?.url || video.snippet?.thumbnails?.default?.url;
-		if (imageUrl) {
-			imageUrl = imageUrl.replace(/\/default\.jpg$/, '/maxresdefault.jpg');
-			const file = Bun.file('next_thumbnail.jpg');
-			try {
-				const result = await fetch(imageUrl);
-				await Bun.write(file, result);
-				if (process.env.NODE_ENV === 'production') {
-					await bot.upload(`File:${titleInfo.mediaTitle}.jpg`, 'next_thumbnail.jpg', `{{Media thumbnail|link=${imageUrl}}}`, {
-						ignorewarning: false,
-						comment: `Automated upload of thumbnail image for YouTube video ${url}`,
-					});
-				} else {
-					log(`[S] (Simulated) Uploaded image: ${imageUrl}`);
-				}
-				await file.delete();
-			} catch (error) {
-				if (error instanceof Error && error.message.includes('already exists')) {
-					log(`[I] Image already exists: ${imageUrl}`);
-					await file.delete();
-					continue;
-				}
-				log(`[E] Error uploading image: ${imageUrl}`);
-				log(error);
-				await file.delete();
-				continue;
-			}
-		}
-
-		if (video.snippet?.publishedAt) {
-			const publishedAt = new Date(video.snippet.publishedAt);
-			await file.write(publishedAt.toISOString());
-		}
+		await saveLastTime(video, file);
 
 		await Bun.sleep(3000);
 	}
@@ -211,5 +249,8 @@ async function main() {
 
 main().then(() => {
 	log('[S] Finished creating video pages.');
+	for (const file of TEMP_FILES) {
+		file.delete();
+	}
 	process.exit();
 });
